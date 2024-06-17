e26e73d96397924960d28f294d8a3f70b6bd138b
MoeMus <144395260+MoeMus@users.noreply.github.com>
2024-06-16 22:08:21 -0700

    Added User and Course models in models.py, which uses TextChoices inner classes

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+model = models.model
 
-# Create your models here.
+class User(AbstractUser):
+    Courses = []
+
+class Course(model):
+    # Semester inner class for defining Semester TextChoices
+    class Semester(models.TextChoices):
+        SPRING = "Spring"
+        SUMMER = "Summer"
+        FALL = "FALL"
+
+        SEMESTER_CHOICES = (
+            (SPRING, 'Spring'),
+            (SUMMER, 'Summer'),
+        )
+    class Component(models.Textchoices):
+        LAB = "Lab"
+        TUTORIAL = "Tutorial"
+        SEMINAR = "Seminar"
+
+    name = ""
+    department = ""
+    course_number = 0
+    professor = ""
+
+    semester = models.Charfield(
+        choices=Semester.choices
+    )
+    component = models.Charfield(
+        choices=Component.choices
+    )
+
+
+

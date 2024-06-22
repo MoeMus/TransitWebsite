867d931d2295fb12f785f6fb66c288ff77bc794e
MoeMus <144395260+MoeMus@users.noreply.github.com>
2024-06-22 13:23:35 -0700

    Created serializers.py structure

---
+++ backend/core/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import User, Course
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+class CourseSerializer(serializers.ModelSerializer):
+

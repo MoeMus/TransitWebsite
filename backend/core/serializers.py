d2714ecd3d2089655d58bc0ab852e23361513355
James Chuong <jcjameschuong@gmail.com>
2024-06-23 13:43:29 -0700

    implement Course Serializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -4,5 +4,8 @@ from .models import User, Course
 
 class UserSerializer(serializers.ModelSerializer):
 
-class CourseSerializer(serializers.ModelSerializer):
 
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ["name", "department", "course_number", "professor", "semester", "component"]

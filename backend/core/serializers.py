2d7f2e6d064c6049e04824fa26a5b4e376c8097f
MoeMus <144395260+MoeMus@users.noreply.github.com>
2024-06-22 21:49:59 -0700

    Created user serializer in serializers.py

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -2,7 +2,21 @@ from rest_framework import serializers
 from .models import User, Course
 
 
+# extra_kwargs is for extra keyword arguments on 'password' to make it 128 characters
 class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        # Password length is set to 128 characters per OWASP
+        # https://owasp.deteact.com/cheat/cheatsheets/Authentication_Cheat_Sheet.html#password-length
+        extra_kwargs = {
+            'password': {'write_only': True, 'max_length': 128},
+            'username': {'max_length': 50}
+        }
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 class CourseSerializer(serializers.ModelSerializer):
 

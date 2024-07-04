54b7183d47d36107018d7de726883ec7d5e8b663
James Chuong <jcjameschuong@gmail.com>
2024-07-04 12:54:10 -0700

    Update add_course_to_user in views.py to search via SFU Course Outline API if course not in local database, add view to filter and retreive all courses with a departmant and course number, create new file utils.py for methods which are used throughout backend, implement methods to retreive current semester

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("add/user/", views.create_new_user, name="add-user"),
     path("/delete/user/<int:id>/", views.delete_user, name="delete-user"),
     path("/delete/user/<int:id>/courses/", views.delete_all_courses, name="delete-all-courses"),
-    path("/add/user/<int:id>/course/<int:course_id>/", views.add_course, name="add-course"),
+    path("/add/user/<int:id>/course/", views.add_course_to_user, name="add-course"),
     path("/delete/user/<int:id>/course/<int:course_id>/", views.remove_course, name="remove-course"),
+    path("/get/courses/filter", views.get_all_courses_with_key_word, name="filter-courses")
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,27 +1,32 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.views.decorators.http import require_POST, require_http_methods
+from django.views.decorators.http import require_POST, require_http_methods, require_GET
 from rest_framework.exceptions import ParseError
-
+import requests
 from .models import User, Course
 from .serializers import CourseSerializer, UserSerializer
 import io
-
+from .utils import *
 from rest_framework.parsers import JSONParser
 
-
 # Create your views here.
+CURRENT_SEMESTER = get_current_semester_code()
+CURRENT_YEAR = get_current_year()
+CURRENT_TERM = get_current_semester_code()
+
 
 @require_POST()
-def create_new_user(json_content, request):
-    stream = io.BytesIO(json_content)
-    data = JSONParser().parse(stream)
-
-    serialized = UserSerializer(data=data)
-    if serialized.is_valid():
-        serialized.save()
-    else:
-        raise ParseError
+def create_new_user(request):
+    stream = io.BytesIO(request.body)
+    try:
+        data = JSONParser().parse(stream)
+        serialized = UserSerializer(data=data)
+        if serialized.is_valid():
+            serialized.save()
+        else:
+            raise ParseError
+    except ParseError:
+        return HttpResponse(status=400)
 
 
 def test_view(request):
@@ -48,15 +53,34 @@ def delete_all_courses(request, pk):
         return HttpResponse(status=404)
 
 
+#Expects the request body to be a JSON representation of a course as defined in the models.py
+#Front-end must create this format
 @require_POST()
-def add_course(request, pk1, pk2):
-    if User.objects.filter(id=pk1).exists() and Course.objects.filter(id=pk2).exists():
+def add_course_to_user(request, pk1):
+    course = None
+    course_id = request.GET.get('pk')
+
+    #If the course doesn't already exist in the database
+    if not Course.objects.filter(id=course_id).exists():
+        stream = io.BytesIO(request.body)
+        try:
+            data = JSONParser().parse(stream)
+            serialized = CourseSerializer(data=data)
+            if serialized.is_valid():
+                serialized.save()
+            else:
+                raise ParseError
+        except ParseError:
+            return HttpResponse(status=400)
+
+    course = Course.objects.get(pk=course_id)
+
+    if User.objects.filter(id=pk1).exists():
         user = User.objects.get(pk=pk1)
-        course = Course.objects.get(pk=pk2)
         user.courses.add(course)
         return HttpResponse(status=200)
     else:
-        return HttpResponse(status=404)
+        return HttpResponse(status=400)
 
 
 @require_http_methods(['DELETE'])
@@ -68,3 +92,33 @@ def remove_course(request, pk1, pk2):
         return HttpResponse(status=200)
     else:
         return HttpResponse(status=404)
+
+
+#Expected request body is a JSON format consisting of {department: <value>, number: <value>}, attributes can be omitted
+@require_GET()
+def get_all_courses_with_key_word(request):
+    department = request.GET.get("department", "")
+    number = request.GET.get("number", "")
+
+    if not department:
+        return
+
+    if not number:
+
+        try:
+            matchingCourse = requests.get(
+                "/bin/wcm/course-outlines?" + CURRENT_YEAR + "/" + CURRENT_TERM + "/" + department)
+            matchingCourse.raise_for_status()
+        except HttpResponse(status=404):
+            return HttpResponse(status=404)
+
+    else:
+
+        try:
+            matchingCourse = requests.get(
+                "/bin/wcm/course-outlines?" + CURRENT_YEAR + "/" + CURRENT_TERM + "/" + department + "/" + number)
+            matchingCourse.raise_for_status()
+        except HttpResponse(status=404):
+            return HttpResponse(status=404)
+
+    return JsonResponse(matchingCourse.json())

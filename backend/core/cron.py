8d158c7d1df3405625531176fa0f356f07ac7892
MoeMus <144395260+MoeMus@users.noreply.github.com>
2024-08-21 21:01:41 -0700

    Created cron.py

---
+++ backend/core/cron.py
@@ -0,0 +1,10 @@
+from django_cron import CronJobBase, Schedule
+from .models import Course
+from .utils import get_current_year, get_current_semester_code
+import requests
+
+class SyncCoursesCronJob(CronJobBase):
+    RUN_EVERY_MINS = 60  # 1 = one minute, 60 = one hour
+    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    code = 'core.sync_courses_cron_job'  # Unique code for cron
+

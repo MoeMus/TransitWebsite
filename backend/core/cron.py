eedc23a16ef17169e30a2631fabbe8592a6b0958
MoeMus <144395260+MoeMus@users.noreply.github.com>
2024-08-23 10:35:55 -0700

    Added course info fields to be created and updated in cron.py

--- backend/core/cron.py
+++ backend/core/cron.py
@@ -4,6 +4,8 @@ from .utils import get_current_year, get_current_semester_code
 import requests
 import logging
 
+logger = logging.getLogger(__name__)
+
 class SyncCoursesCronJob(CronJobBase):
     RUN_EVERY_MINS = 60  # 1 = one minute, 60 = one hour
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
@@ -23,9 +25,27 @@ class SyncCoursesCronJob(CronJobBase):
 
                 for course_info in courses:
                     Course.objects.update_or_create(
-                        name=course_info.get("name")
+                        name=course_info.get("name"),
+                        defaults={ # TODO: Update course model with the given fields below
+                            "department": course_info.get("dept"),
+                            "course_number": course_info.get("number"),
+                            "title": course_info.get("title"),
+                            "section_name": course_info.get("section"),
+                            "description": course_info.get("description"),
+                            "term": course_info.get("term"),
+                            "delivery_method": course_info.get("deliveryMethod"),
+                            "start_time": course_info.get("startTime"),
+                            "start_date": course_info.get("startDate"),
+                            "end_time": course_info.get("endTime"),
+                            "end_date": course_info.get("endDate"),
+                            "is_exam": course_info.get("isExam"),
+                            "days": course_info.get("days"),
+                            "campus": course_info.get("campus"),
+                        },
                 )
+
             except requests.exceptions.RequestException as err:
                 logger.error(f"Could not sync courses for {department}: {err}")
+
     def get_departments(self):
         return ['CMPT'] #TODO: Change the list of departments

14ed009635bb333805d9e61ab92aab7b427ae800
MoeMus <144395260+MoeMus@users.noreply.github.com>
2024-08-28 17:38:44 -0700

    Added more fields in the CourseSection model in models.py

--- backend/core/models.py
+++ backend/core/models.py
@@ -61,6 +61,13 @@ class CourseSection(models.Model):
     class_type = models.CharField(max_length=10)  # e.g., "e" or "n"
     associated_class = models.CharField(max_length=10)  # e.g., "1"
     title = models.CharField(max_length=100)
+    start_time = models.CharField(max_length=100, null=True, blank=True)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_time = models.CharField(max_length=100, null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    is_exam = models.BooleanField(default=False)
+    days = models.CharField(max_length=100, null=True, blank=True)
+    campus = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.course.title} - {self.section_code} ({self.class_type})"

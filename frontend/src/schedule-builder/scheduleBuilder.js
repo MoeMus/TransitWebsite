8292c4ddb60ba193295b4f2e4f8fb06f7939c37c
MoeMus <144395260+MoeMus@users.noreply.github.com>
2024-09-03 20:33:47 -0700

    Added function for fetching courses in ScheduleBuilder.js and the Javascript XML for the component

--- frontend/src/schedule-builder/scheduleBuilder.js
+++ frontend/src/schedule-builder/scheduleBuilder.js
@@ -1,11 +1,42 @@
 import React, { useState, useEffect } from "react";
 
-export function ScheduleBuilder() {
-  const [availableCourses, setAvailableCourses] = useState([]);
-  const [selectedCourses, setSelectedCourses] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
 
-  useEffect(() => {
-    fetchAvailableCourses();
-  }, []);
+export function ScheduleBuilder() {
+    const [availableCourses, setAvailableCourses] = useState([]);
+    const [selectedCourses, setSelectedCourses] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        fetchAvailableCourses();
+    }, []);
+
+    const fetchAvailableCourses() = async () => {
+        try {
+        } catch (err) {
+
+        setError(err)
+        }
+        }
+
+    }
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+      if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return (
+    <>
+      <Toaster position="top-left" reverseOrder={false} />
+      <Container>
+        <h2>Schedule Builder</h2>
+
+      </Container>
+    </>
+  );
+}
+

aa86951c349f5c163a4a1caf797b24959ed10d12
MoeMus <--replace-all>
2025-12-20 15:41:39 -0800

    Get user from request object and add blacklisting tokens

--- backend/core/api_endpoints/user_views.py
+++ backend/core/api_endpoints/user_views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
 
 from core.models import User, LectureSection, NonLectureSection
 from core.serializers import UserSerializer, LectureSectionSerializer, NonLectureSectionSerializer
@@ -18,14 +19,8 @@ class UserView(APIView):
     @permission_classes([IsAuthenticated])
     def get(self, request):
 
-        username = request.query_params.get('username')
-
-        try:
-            current_user = User.objects.get(username=username)
-            serializer = UserSerializer(current_user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except User.DoesNotExist:
-            return Response({"error": "User: " + username + " not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
 
@@ -47,14 +42,11 @@ class UserView(APIView):
     @permission_classes([IsAuthenticated])
     def delete(self, request):
 
-        username = request.query_params.get('username', None)
-        try:
-            user = User.objects.get(username=username)
-            user.delete()
-            return Response(status=status.HTTP_200_OK)
+        for token in OutstandingToken.objects.filter(user=request.user):
+            BlacklistedToken.objects.get_or_create(token=token)
 
-        except User.DoesNotExist:
-            return Response({"error": "Account not found"}, status=status.HTTP_404_NOT_FOUND)
+        request.user.delete()
+        return Response(status=status.HTTP_200_OK)
 
     @permission_classes([IsAuthenticated])
     def put(self, request):
@@ -63,17 +55,14 @@ class UserView(APIView):
 
             with transaction.atomic():
 
-                user = User.objects.get(username=request.data['username'])
-
-                if user.DoesNotExist:
-
-                    return Response({"error": "Account not found"}, status=status.HTTP_404_NOT_FOUND)
-
+                user = request.user
                 serializer = UserSerializer(user, data=request.data)
 
                 if serializer.is_valid():
-
                     serializer.save()
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+                else:
+                    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         except IntegrityError:
 
@@ -83,11 +72,10 @@ class UserView(APIView):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def get_user_courses(request, username):
+def get_user_courses(request, username=None):
 
     try:
-
-        user = get_object_or_404(User, username=username)
+        user = request.user
         lecture_sections = user.lecture_sections.all()
         non_lecture_sections = user.non_lecture_sections.all()
 
@@ -111,14 +99,11 @@ def remove_courses(request):
     try:
 
         with transaction.atomic():
-            user = User.objects.get(username=request.data["username"])
+            user = request.user
             user.lecture_sections.clear()
             user.non_lecture_sections.clear()
             user.save()
             return Response({"success": "All courses removed from schedule"}, status=status.HTTP_200_OK)
-
-    except User.DoesNotExist:
-        return Response({"error": "User does not exist"}, status=status.HTTP_404_NOT_FOUND)
 
 
 # Adds a course (lecture section or non lecture section) to a user's schedule
@@ -127,14 +112,11 @@ def remove_courses(request):
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def add_course_to_schedule(request):
-    username = request.data['username']
     department = request.data["department"]
     course_number = request.data["course_number"]
     section_code = request.data["section_code"]
 
-    try:
-
-        user = get_object_or_404(User, username=username)
+    user = request.user
 
         # Get the user's courses by username
         user_courses = list(user.lecture_sections.all())
@@ -192,24 +174,16 @@ def add_course_to_schedule(request):
 
         return Response({"success": "Section added successfully"}, status=status.HTTP_200_OK)
 
-    except Http404 as e:
-
-        return Response({"error": "No user found"}, status=status.HTTP_404_NOT_FOUND)
-
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def remove_course_from_schedule(request):
-    username = request.data['username']
     department = request.data["department"]
     course_number = request.data["course_number"]
     section_code = request.data["section_code"]
 
-    try:
-
-        with transaction.atomic():
-
-            user = get_object_or_404(User, username=username)
+    with transaction.atomic():
+            user = request.user
 
             lecture_section = LectureSection.objects.filter(department=department, number=course_number,
                                                             section_code=section_code).first()
@@ -233,8 +207,3 @@ def remove_course_from_schedule(request):
             user.save()
 
             return Response({"success": "Section removed successfully"}, status=status.HTTP_200_OK)
-
-    except Http404 as e:
-
-        return Response({"error": "course could not be removed from your schedule"},
-                        status=status.HTTP_404_NOT_FOUND)
